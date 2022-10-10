b0b6f5dc1724e8d189c818a1b8c117b4854e2c7d
James Knight <contact@mtn-studio.com>
2022-10-10 10:28:14 -0700

    Added initial player objects and game board idea.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Gameboard
+const gameBoard = () => {
+	let board = [];
+
+	// Player creation
+	const playerFactory = (name, marker, turn) => {
+		return name, marker, turn;
+	};
+
+	const player1 = playerFactory('Player 1', 'X', true);
+	const player2 = playerFactory('Player 2', 'O', false);
+};
