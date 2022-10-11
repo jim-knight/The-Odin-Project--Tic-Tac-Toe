4d78da656bf74af48ce61761b52561534da43e42
James Knight <contact@mtn-studio.com>
2022-10-11 10:55:47 -0700

    Board markers  to array added. Added in win logic that actually works!

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Reference
-//https://github.com/ken862734801/Tic-Tac-Toe/blob/main/script.js
-
 // Players module
 const players = (() => {
 	// Player creation factory function
@@ -14,32 +11,96 @@ const players = (() => {
 	let currentPlayer = player1;
 
 	let switchActivePlayer = () => {
-		if (players.currentPlayer === players.player1) {
-			players.currentPlayer = players.player2;
-			console.log(`Next turn: Player 2`);
-		} else {
-			players.currentPlayer = players.player1;
-			console.log(`Next turn: Player 1`);
-		}
+		players.currentPlayer === players.player1
+			? (players.currentPlayer = players.player2)
+			: (players.currentPlayer = players.player1);
 	};
-
 	return { player1, player2, currentPlayer, switchActivePlayer };
 })();
 
 // Game variables module
 const gameBoard = (() => {
+	let gameOver = false;
+
 	// Generating the board array
 	let board = new Array(9).fill('');
 
-	let gameOver = false;
+	const boardMarker = (marker, i) => {
+		board[i] = marker;
+	};
 
-	return { board, gameOver };
+	const resetBoard = () => board.fill('');
+
+	// Win conditions
+	const winConditions = () => {
+		const square = gameBoard.board;
+		const curPlayer = players.currentPlayer.name;
+		// Horizontals
+		if (
+			(square[0] == curPlayer &&
+				square[1] == curPlayer &&
+				square[2] == curPlayer) ||
+			(square[3] == curPlayer &&
+				square[4] == curPlayer &&
+				square[5] == curPlayer) ||
+			(square[6] == curPlayer &&
+				square[7] == curPlayer &&
+				square[8] == curPlayer)
+		) {
+			console.log('You win with a horizontal line!');
+		}
+		// Verticals
+		else if (
+			(square[0] == curPlayer &&
+				square[3] == curPlayer &&
+				square[6] == curPlayer) ||
+			(square[1] == curPlayer &&
+				square[4] == curPlayer &&
+				square[7] == curPlayer) ||
+			(square[2] == curPlayer &&
+				square[5] == curPlayer &&
+				square[8] == curPlayer)
+		) {
+			console.log('You win with a vertical line!');
+		}
+		// Diagonals
+		else if (
+			(square[0] == curPlayer &&
+				square[4] == curPlayer &&
+				square[8] == curPlayer) ||
+			(square[2] == curPlayer &&
+				square[4] == curPlayer &&
+				square[6] == curPlayer)
+		) {
+			console.log('You win with a diagonal line!');
+		}
+	};
+
+	// Draw condition
+	const drawCondition = () => {
+		// Checks to find the first empty square, which means a valid play space is still available
+		let emptySquare = board.indexOf('');
+		if (emptySquare == -1) {
+			gameOver = true;
+			console.log('All out of play spaces');
+		}
+	};
+
+	return {
+		board,
+		boardMarker,
+		drawCondition,
+		gameOver,
+		resetBoard,
+		winConditions,
+	};
 })();
 
 // DOM manipulation module
 const displayController = (() => {
 	// DOM elements
 	const squares = document.querySelectorAll('.squares');
+
 	// const messageDisplay
 	// const refreshBtn
 
@@ -50,6 +111,12 @@ const displayController = (() => {
 		});
 	});
 
+	// Get index position of current square
+	const squareIndex = (e) => {
+		let sqIndex = e.target.dataset.id;
+		return sqIndex;
+	};
+
 	const drawSquare = (e) => {
 		// Check if this square is already taken
 		if (e.target.innerHTML) {
@@ -59,10 +126,14 @@ const displayController = (() => {
 		}
 		e.target.innerHTML = `<i class="fa-regular fa-${players.currentPlayer.marker}"></i>`;
 		e.target.dataset.owner = players.currentPlayer.name;
+		gameBoard.boardMarker(players.currentPlayer.name, squareIndex(e));
+		gameBoard.winConditions();
+		gameBoard.drawCondition();
 		players.switchActivePlayer();
 	};
 
 	// Message
-
 	// Clear game screen
+
+	return { squareIndex };
 })();
